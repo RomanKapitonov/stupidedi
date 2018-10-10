9ecb7127a91673a35d1e159d14d98ebb8dc2b2aa
Roman Kapitonov <RomanKapitonov@coherentsolutions.com>
2018-10-10 15:00:45 +0300

    An example of how parsed tree may be converted to json-like format (hash).

--- lib/stupidedi/writer.rb
+++ lib/stupidedi/writer.rb
@@ -4,5 +4,18 @@ module Stupidedi
   module Writer
     autoload :Claredi,  "stupidedi/writer/claredi"
     autoload :Default,  "stupidedi/writer/default"
+    autoload :Json,     "stupidedi/writer/json"
+
+    class Json
+      autoload :Transmission,     "stupidedi/writer/json/transmission"
+      autoload :Interchange,      "stupidedi/writer/json/interchange"
+      autoload :FunctionalGroup,  "stupidedi/writer/json/functional_group"
+      autoload :TransactionSet,   "stupidedi/writer/json/transaction_set"
+      autoload :Table,            "stupidedi/writer/json/table"
+      autoload :Loop,             "stupidedi/writer/json/loop"
+      autoload :Segment,          "stupidedi/writer/json/segment"
+      autoload :Element,          "stupidedi/writer/json/element"
+      autoload :NullNode,         "stupidedi/writer/json/null_node"
+    end
   end
 end
